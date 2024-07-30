89abfcf2593de32d88b936c14da9538c0b4572e5
Ali Mahani <ali.a.mahani@zoho.com>
2024-07-30 17:54:59 -0600

    formatt the which-key.lua file

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -1,24 +1,24 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
+    "folke/which-key.nvim",
+    event = "VeryLazy",
     dependencies = {
         { 'echasnovski/mini.icons',
             version = false,
             opts = {},
         },
     },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
     },
-  },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
 }
