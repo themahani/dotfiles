829ab6127090443a8c89a9ccbc4d6d6461293547
Ali Mahani <ali.a.mahani@zoho.com>
2024-08-17 17:32:36 -0600

    add bufferline plugin to neovim.

---
+++ config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+-- Show a list of buffers at the top of the page
+return {
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("bufferline").setup()
+        end
+    }
+}
--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -1,7 +1,9 @@
+-- Plugins to enhance the editor workflow
 local editor_plugins = {
-    "tpope/vim-commentary",
-    "tpope/vim-surround",
-    "tpope/vim-repeat",
+    "tpope/vim-commentary", -- 
+    "tpope/vim-surround",   -- Work better with surrounding pairs {(<>)}
+    "tpope/vim-repeat",     -- Enhance repeat <.> capabilities
+    "jiangmiao/auto-pairs", -- Add automatic pairing {(<>)}
 }
 
 return editor_plugins
