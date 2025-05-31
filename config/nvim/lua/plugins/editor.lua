5ca54105223364349302241ec17795e46d1df6dd
Ali Mahani <ali.a.mahani@zoho.com>
2025-05-31 15:42:13 -0600

    fix(nvim): switch to nvim-autopairs

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -3,8 +3,15 @@ local editor_plugins = {
   "tpope/vim-commentary", --
   "tpope/vim-surround", -- Work better with surrounding pairs {(<>)}
   "tpope/vim-repeat", -- Enhance repeat <.> capabilities
-  "jiangmiao/auto-pairs", -- Add automatic pairing {(<>)}
+  -- "jiangmiao/auto-pairs", -- Add automatic pairing {(<>)}
   "tpope/vim-sleuth", -- Auto shift tabsize and tab width
+  { -- Auto pairs for neovim
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
 }
 
 return editor_plugins
