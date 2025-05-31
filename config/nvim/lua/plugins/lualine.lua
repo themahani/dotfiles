5de3488faadf84bb855cae398d78d5608b9529d0
Ali Mahani <ali.a.mahani@zoho.com>
2025-05-31 15:42:58 -0600

    fix(nvim): customize lualine to show buffers and tabs

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -8,6 +8,7 @@ return {
         icons_enabled = true,
         section_separators = { left = "", right = "" },
         component_separators = { left = "", right = "" },
+        globalstatus = true,
       },
       sections = {
         lualine_a = { "mode" },
@@ -25,7 +26,93 @@ return {
         lualine_y = {},
         lualine_z = {},
       },
-      tabline = {},
+      tabline = {
+        lualine_a = {
+          {
+            "tabs",
+            tab_max_length = 40, -- Maximum width of each tab. The content will be shorten dynamically (example: apple/orange -> a/orange)
+            max_length = vim.o.columns / 2, -- Maximum width of tabs component.
+            -- Note:
+            -- It can also be a function that returns
+            -- the value of `max_length` dynamically.
+            mode = 2, -- 0: Shows tab_nr
+            -- 1: Shows tab_name
+            -- 2: Shows tab_nr + tab_name
+
+            path = 0, -- 0: just shows the filename
+            -- 1: shows the relative path and shorten $HOME to ~
+            -- 2: shows the full path
+            -- 3: shows the full path and shorten $HOME to ~
+
+            -- Automatically updates active tab color to match color of other components (will be overidden if buffers_color is set)
+            use_mode_colors = false,
+
+            tabs_color = {
+              -- Same values as the general color option can be used here.
+              active = "lualine_a_normal", -- Color for active tab.
+              inactive = "lualine_a_inactive", -- Color for inactive tab.
+            },
+
+            show_modified_status = true, -- Shows a symbol next to the tab name if the file has been modified.
+            symbols = {
+              modified = "[+]", -- Text to show when the file is modified.
+            },
+
+            fmt = function(name, context)
+              -- Show + if buffer is modified in tab
+              local buflist = vim.fn.tabpagebuflist(context.tabnr)
+              local winnr = vim.fn.tabpagewinnr(context.tabnr)
+              local bufnr = buflist[winnr]
+              local mod = vim.fn.getbufvar(bufnr, "&mod")
+
+              return name .. (mod == 1 and " +" or "")
+            end,
+          },
+        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {
+          {
+            "buffers",
+            show_filename_only = true,
+            hide_filename_extension = false,
+            show_modified_status = true,
+            mode = 0, -- 0: Shows buffer name
+            -- 1: Shows buffer index
+            -- 2: Shows buffer name + buffer index
+            -- 3: Shows buffer number
+            -- 4: Shows buffer name + buffer number
+
+            max_length = vim.o.columns / 2, -- Maximum width of buffers component,
+            -- it can also be a function that returns
+            -- the value of `max_length` dynamically.
+            filetype_names = {
+              TelescopePrompt = "Telescope",
+              dashboard = "Dashboard",
+              packer = "Packer",
+              fzf = "FZF",
+              alpha = "Alpha",
+            }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
+
+            -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
+            use_mode_colors = true,
+
+            -- buffers_color = {
+            --   -- Same values as the general color option can be used here.
+            --   active = "lualine_z_normal", -- Color for active buffer.
+            --   inactive = "lualine_z_inactive", -- Color for inactive buffer.
+            -- },
+
+            symbols = {
+              modified = " ●", -- Text to show when the buffer is modified
+              alternate_file = "#", -- Text to show to identify the alternate file
+              directory = "", -- Text to show when the buffer is a directory
+            },
+          },
+        },
+      },
       winbar = {},
       inactive_winbar = {},
       extensions = {},
