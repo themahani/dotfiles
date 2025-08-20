bbd3d8870daa392bc237b25e0eedf5d893aac592
Ali Mahani <ali.a.mahani@zoho.com>
2025-08-19 23:51:17 -0600

    fix: switch from swww to hyprpaper

--- config/hypr/scripts/rofi-hyprpaper.sh
+++ config/hypr/scripts/rofi-hyprpaper.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Directory containing your wallpapers
-WALLPAPER_DIR="$HOME/.wallpapers/"
+WALLPAPER_DIR="$HOME/.wallpapers"
 ROFI_CONFIG_DIR="$HOME/.config/rofi"
+CURRENT_WALLPAPER="$WALLPAPER_DIR/.current"
 
 # Get a list of all image files in the directory
-WALLPAPERS=($(find "$WALLPAPER_DIR" -type f -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" ))
+WALLPAPERS=($(find "$WALLPAPER_DIR" -type f -name "*.jpg" -o -name "*.jpeg" -o -name "*.png"))
 
 # Check if there are any wallpapers
 if [ ${#WALLPAPERS[@]} -eq 0 ]; then
@@ -16,7 +17,7 @@ fi
 # Create a rofi menu with wallpaper previews
 MENU=""
 for wallpaper in "${WALLPAPERS[@]}"; do
-    MENU+="$(basename "$wallpaper")\0icon\x1f$wallpaper\n"
+    MENU+="$(basename $wallpaper)\0icon\x1f$wallpaper\n"
 done
 
 # Show the rofi menu and get the selected wallpaper
@@ -24,6 +25,9 @@ SELECTION=$(echo -en "$MENU" | rofi -dmenu -i -show-icons -theme "$ROFI_CONFIG_D
 
 # If a wallpaper was selected, set it with swww
 if [ -n "$SELECTION" ]; then
-    SELECTED_WALLPAPER="$WALLPAPER_DIR$SELECTION"
-    swww img "$SELECTED_WALLPAPER" --transition-type any --transition-fps 60
+    SELECTED_WALLPAPER="$WALLPAPER_DIR/$SELECTION"
+    ln -sf "$SELECTED_WALLPAPER" "$CURRENT_WALLPAPER"   # Create symlink
+    hyprctl hyprpaper preload "$CURRENT_WALLPAPER"
+    hyprctl hyprpaper reload ",$CURRENT_WALLPAPER"
+    hyprctl hyprpaper unload unused
 fi
