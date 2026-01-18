f0b6ee7135ab714ee32c3158e8e26cfdd6437e6d
Ali Mahani <ali.a.mahani@zoho.com>
2026-01-18 16:43:07 -0700

    fix: make neotree toggle on <C-n> key combination

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -21,6 +21,6 @@ return {
         },
       },
     })
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>", {})
   end,
 }
