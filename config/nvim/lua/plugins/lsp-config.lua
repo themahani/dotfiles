ca5ce5440d65bf242a2eefeee7d77352b1143578
Ali Mahani <ali.a.mahani@zoho.com>
2026-06-07 16:51:20 -0600

    clean(nvim): remove obsolete packs since v0.12

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -39,7 +39,7 @@ return {
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = "enter" },
+        keymap = { preset = "tab" },
 
         appearance = {
           -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
